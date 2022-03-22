749684d45d41a2a4584793472ae81f677819624a
Aman <amansinha@licious.com>
2022-03-22 18:52:29 +0530

    Initial Draft - Fetching Title

---
+++ routes/wordle.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const {getSpreadSheet} = require("../api/helper/gsheet");
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/', async function (req, res, next) {
+    res.send('Wordle route is working ');
+});
+
+router.get('/sheetTitle', async function (req, res, next) {
+    let data = getSpreadSheet();
+    res.send('Wordle route is working ' + data);
+});
+
+module.exports = router;
